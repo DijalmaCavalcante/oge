4e6b08b1f922456fe7ba3019b6acd12b7c673341
dijalmacavalcante <dijalma0412@gmail.com>
2025-07-25 16:22:46 -0300

    chore: estruturando a pagina de ferramenta do jogo

--- src/games/cidadedorme/index.jsx
+++ src/games/cidadedorme/index.jsx
@@ -1,11 +1,41 @@
- import Container from '@elements/container'
- 
- export default function cidadedorme () {
+import './style.css'
+import Backheader from '@elements/backheader'
+import Button from '@elements/button'
+import Container from '@elements/container'
+
+import somAssassino from './assassino.mp3'
+import somAnjo from './anjo.mp3'
+import somDetetive from './detetive.mp3'
+import { useState } from 'react'
+
+export default function cidadedorme () {
+  const [audioAtual, setAudioAtual] = useState(null)
+
+  function playOrPause(som) {
+    if (audioAtual && audioAtual.src === som && !audioAtual.paused) {
+      audioAtual.pause()
+      return
+    }
+
+    if (audioAtual) {
+      audioAtual.pause()
+    }
+
+    const novoAudio = new Audio(som)
+    novoAudio.play()
+    setAudioAtual(novoAudio)
+  }
+
   return (
-    <Container>
-      <button id='assassino'>Assassino</button>
-      <button id='anjo'>Anjo</button>
-      <button id='detetive'>Detetive</button>
-    </Container>
+    <div className="cidadedorme">
+      <Backheader />
+      <div className='cidadedorme__container'>
+        <Container>
+          <Button id='assassino' onClick={() => playOrPause(somAssassino)}>Assassino</Button>
+          <Button id='anjo' onClick={() => playOrPause(somAnjo)}>Anjo</Button>
+          <Button id='detetive' onClick={() => playOrPause(somDetetive)}>Detetive</Button>
+        </Container>
+      </div>
+    </div>
   )
- }
+}

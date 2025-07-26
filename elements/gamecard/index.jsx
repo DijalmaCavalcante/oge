f2b90ec435945afcabd34658a424953c0e4e1971
dijalmacavalcante <dijalma0412@gmail.com>
2025-07-26 10:33:05 -0300

    chore: deixando o estilo do gamecard mais dinamico

--- elements/gamecard/index.jsx
+++ elements/gamecard/index.jsx
@@ -1,9 +1,13 @@
 import './style.css'
 
-export default function gamecard ({ children }) {
+export default function gamecard (props) {
+  const style = {
+    '--card-bg': `var(--color-${props.color || 'master'})`
+  }
+
   return (
-    <div className="gamecard">
-      {children}
+    <div className="gamecard" style={style}>
+      {props.children}
     </div>
   )
 }

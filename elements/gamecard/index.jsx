2b9df078c7b6f57bcccedc67f62e6be38e2c364f
dijalmacavalcante <dijalma0412@gmail.com>
2025-07-26 21:26:58 -0300

    chore: tras dinamicidade e melhora style

--- elements/gamecard/index.jsx
+++ elements/gamecard/index.jsx
@@ -2,11 +2,11 @@ import './style.css'
 
 export default function gamecard (props) {
   const style = {
-    '--card-bg': `var(--color-${props.color || 'master'})`
+    '--card-bg': `var(--color-${props.color || 'master'})`,
   }
 
   return (
-    <div className="gamecard" style={style}>
+    <div className="gamecard" style={style} pointer={props.pointer}>
       {props.children}
     </div>
   )

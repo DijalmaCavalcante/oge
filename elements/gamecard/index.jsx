ff606f165945662702c4cad67b17a11fbbba263c
dijalmacavalcante <dijalma0412@gmail.com>
2025-07-28 19:33:59 -0300

    feat: adiciona layout flexivel ao meu component gamecard

--- elements/gamecard/index.jsx
+++ elements/gamecard/index.jsx
@@ -1,12 +1,28 @@
 import './style.css'
 
 export default function gamecard (props) {
+  function imagesize () {
+    const sizes = {
+      nano: '50px',
+      small: '100px',
+      medium: '150px',
+      large: '200px'
+    }
+    return sizes[props.imageSize] || sizes.large
+  }
+
   const style = {
     '--card-bg': `var(--color-${props.color || 'master'})`,
+    '--card-pointer': `${props.pointer ? 'pointer' : ''}`,
+    '--card-radius': `var(--${props.radius || 'radius-medium'}`,
+    '--card-justify': `${props.justify || 'center'}`,
+    '--card-direction': `${props.row || 'column'}`,
+    '--card-image-size': `${imagesize()}`,
+    '--card-width': `${props.wide ? '100%' : 'auto'}`
   }
 
   return (
-    <div className="gamecard" style={style} pointer={props.pointer}>
+    <div className="gamecard" style={style} onClick={props.onClick}>
       {props.children}
     </div>
   )

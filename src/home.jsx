c733b9536d574e08ce0c817e638dd64061a319b5
dijalmacavalcante <dijalma0412@gmail.com>
2025-08-04 15:04:52 -0300

    chore: melhora estrutura do css

--- src/home.jsx
+++ src/home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main>
       <Header />
       <div className="home__container">
-        <Container align='start'>
+        <Container>
           <Games />
         </Container>
       </div>

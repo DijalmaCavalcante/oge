12f625617c66725ea25186433723d61c1a46e8c6
dijalmacavalcante <dijalma0412@gmail.com>
2025-08-06 17:21:30 -0300

    chore: melhorando a estrutura da lista de games

--- src/games/index.jsx
+++ src/games/index.jsx
@@ -5,10 +5,10 @@ import Container from '@elements/container'
 
 export default function Home() {
   const flexdirection = window.innerWidth >= 768 ? 'row' : ''
-
+  
   return (
     <main>
-      <Container row={flexdirection}>
+      <Container row={flexdirection} wrap>
         {gamelist.map((game) => (
           <Gamecard key={game.id}>
             <img src={game.image} />

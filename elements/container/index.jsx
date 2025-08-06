53ba96f99654b111e87779cfa917f146ed023015
dijalmacavalcante <dijalma0412@gmail.com>
2025-08-06 17:21:16 -0300

    feat: trazendo novas funcionanlidades co container

--- elements/container/index.jsx
+++ elements/container/index.jsx
@@ -1,10 +1,13 @@
 import './style.css'
 
 export default function container (props) {
+
   const style = {
     '--container-direction': `${props.row || 'column'}`,
     '--container-justify': `${props.justify || 'center'}`,
-    '--container-align': `${props.align || 'center'}`
+    '--container-align': `${props.align || 'center'}`,
+    '--container-wrap': `${props.wrap ? 'wrap' : ''}`,
+    '--container-gap': `var(--spacing_inset-${props.gap || 'xs'})`
   }
 
   return (
